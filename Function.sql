5c3c13eb61307b076f51d0ff4dc8bf53ca413b70
Alauddinkhilji <41534633+Alauddinkhilji@users.noreply.github.com>
2018-08-05 19:29:56 +0530

    Update Function.sql

--- Function.sql
+++ Function.sql
@@ -60,3 +60,36 @@ end loop;
 end if;
 return total;
 end;
+
+
+
+
+
+create or replace function ganeshk(code number)
+return number is
+    total number;
+	scode student_masters.student_code%type;
+	sname student_masters.student_name%type;
+	dname department_masters.dept_name%type;
+	dcode department_masters.dept_code%type;
+	cursor cur is 
+	    select s.student_code, s.student_name,s.dept_code,d.dept_name from student_masters s join department_masters d on s.dept_code=d.dept_code; 
+		
+	begin
+	select count(*) into total
+    from student_masters
+    where dept_code=code;
+		open cur;
+		fetch cur into scode,sname,dcode,dname;
+		loop
+			exit when cur % notfound;
+			if dcode is null then
+            dbms_output.put_line('Student id' || scode ||' is null');
+            else
+			dbms_output.put_line(scode || sname ||dcode||dname);
+			fetch cur into scode,sname,dcode,dname;
+			end if;
+		end loop;
+	close cur;	
+	return total;
+	end;
